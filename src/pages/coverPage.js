eb97bfa42bd16be1f185690d88d0f75a4cdbc0d7
ardszsantos <lokgamer01@gmail.com>
2024-10-22 16:13:46 -0300

    feat: created the cover page with the placeholder for the 2 main blocks.

---
+++ src/pages/coverPage.js
@@ -0,0 +1,14 @@
+const CoverPage = () => {
+    return (  
+        <div className="flex gap-[36px] bg-custom-gradient min-h-screen w-full pt-[55px] pl-[350px] pb-[55px] pr-[350px]">
+            <div className="bg-white rounded-[30px] w-[592px] h-[858px]">
+
+            </div>
+            <div className="bg-white self-start rounded-[30px] w-[592px] h-[881px] -mt-[23px]">
+
+            </div>
+        </div>
+    );
+}
+ 
+export default CoverPage;
