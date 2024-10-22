be235dc783a80656f1eaacd652764d30fa1cb4bf
ardszsantos <lokgamer01@gmail.com>
2024-10-22 15:00:22 -0300

    startingb

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-dark': '#222D38',
+        'light-dark': '#46586B',
+      },
+    },
   },
   plugins: [],
 }
