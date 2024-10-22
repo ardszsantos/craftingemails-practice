69b60536bb69894709c83bc1099abbe8dae63892
ardszsantos <lokgamer01@gmail.com>
2024-10-22 16:12:58 -0300

    styling: added a gradient color for the cover page background

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
         'custom-dark': '#222D38',
         'light-dark': '#46586B',
       },
+      backgroundImage: {
+        'custom-gradient': 'linear-gradient(90deg, #429A84 0%, #31E9A7 100%)',
+      },
     },
   },
   plugins: [],
