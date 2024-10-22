1594a40652028f609b7b4a1fe61843f3242a6696
ardszsantos <lokgamer01@gmail.com>
2024-10-22 16:13:30 -0300

    feat: implemented the router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import FirstPage from './pages/firstPage';
+import CoverPage from './pages/coverPage';
 
 function App() {
   return (
-    <div className="flex flex-col h-full items-center justify-center bg-gray-200">
-      <FirstPage/>
-    </div>
+    <Router>
+      <div className="flex flex-col h-full items-center justify-center">
+        <Routes>
+          <Route path="/" element={<FirstPage />} />
+          <Route path="/cover" element={<CoverPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
